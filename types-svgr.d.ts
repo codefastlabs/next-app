448c76536e5694c259861d5c2bf3222c4295749e
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-09 20:56:29 +0700

    feat: add SVG handling with @svgr/webpack and config updates
    Introduced custom webpack configuration to handle SVG imports using @svgr/webpack. Updated tsconfig to include types for SVG and added necessary dependencies in pnpm-lock file.

---
+++ types-svgr.d.ts
@@ -0,0 +1,13 @@
+declare module '*.svg' {
+  import {type FC, type SVGProps} from 'react';
+
+  const content: FC<SVGProps<SVGElement>>;
+
+  export default content;
+}
+
+declare module '*.svg?url' {
+  const content: never;
+
+  export default content;
+}
