cfa83e07db56d6af199f4f9c0b041051eb737a24
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-10-09 21:52:29 +0700

    chore: update pnpm-lock with new dependencies
    Added several new dependencies, including @codefast/hooks, @codefast/ui, and various @radix-ui packages. These dependencies include peer dependencies for react and react-dom, with version compatibility ranging from 16 to 19.0.0-rc.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@codefast/ui/**/*.map',
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   presets: [sharedConfig],
