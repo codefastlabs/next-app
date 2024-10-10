8fd352dae44a8b977e4f4dc8dd9f32cc8e38e01b
BM Valentino <valentino@brickmate.vn>
2024-10-10 09:43:50 +0700

    feat(testing): add Jest configuration and initial tests
    Set up Jest with initial configuration for Next.js. Included test for the 'Page' component using Testing Library and essential jest-dom enhancements.

---
+++ __tests__/page.test.jsx.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Page from '../app/page';
+
+test('renders the Page component', () => {
+  render(<Page />);
+
+  // Check if the Next.js logo is present
+  const logo = screen.getByAltText('Next.js logo');
+
+  expect(logo).toBeInTheDocument();
+
+  // Check if the "Get started by editing" text is present
+  const getStartedText = screen.getByText(/Get started by editing/i);
+
+  expect(getStartedText).toBeInTheDocument();
+
+  // Check if the "Deploy now" link is present
+  const deployLink = screen.getByText(/Deploy now/i);
+
+  expect(deployLink).toBeInTheDocument();
+
+  // Check if the "Read our docs" link is present
+  const docsLink = screen.getByText(/Read our docs/i);
+
+  expect(docsLink).toBeInTheDocument();
+});
---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import nextJest from 'next/jest';
+import { type Config } from 'jest';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const config: Config = {
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'jsdom',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[tj]s?(x)'],
+};
+
+export default createJestConfig(config);
