6b960a34d348328bec88ac4b42951bc74c97ed90
BM Valentino <valentino@brickmate.vn>
2024-10-29 10:07:24 +0700

    refactor(tailwind): update content paths for codefast UI
    Replaced the content path for codefast UI from '*.map' files to '*.js' files to correctly include the relevant JavaScript files. This enhances the accuracy of TailwindCSS purging by targeting the correct assets within the node_modules directory.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
 import sharedConfig from '@codefast/config-tailwind';
+
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@codefast/ui/**/*.map',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@codefast/ui/**/*.js',
   ],
   presets: [sharedConfig],
   theme: {
