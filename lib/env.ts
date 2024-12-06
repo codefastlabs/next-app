802dde0e68ed7d88ec69cbf770349013eb366045
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-12-06 22:15:23 +0700

    chore(deps): update pnpm lock file and version dependencies
    Updated various package versions in pnpm-lock.yaml, including react, @codefast hooks, ui, and others. This also includes updates to dependencies and peer dependencies for maintainability and consistency across the project.

---
+++ lib/env.ts
@@ -0,0 +1,22 @@
+import { object, string, ZodError, type ZodSchema } from 'zod';
+
+import { logger } from '@/lib/logger';
+
+const environmentSchema = object({
+  DATABASE_URL: string().min(1).url(),
+});
+
+const validateEnvironment = <T>(schema: ZodSchema<T>, env: NodeJS.ProcessEnv): T => {
+  try {
+    return schema.parse(env);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      logger.error('Environment validation failed:\n', JSON.stringify(error.errors, null, 2));
+      process.exit(1);
+    }
+
+    throw new Error('Unreachable code after process.exit');
+  }
+};
+
+export const env = validateEnvironment(environmentSchema, process.env);
