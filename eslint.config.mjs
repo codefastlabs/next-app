b4739c366f67c0330edb537fc06971a82fbf04f7
BM Valentino <valentino@brickmate.vn>
2024-12-10 13:35:52 +0700

    chore: upgrade Codefast packages and Playwright
    Updated dependencies to the latest versions, particularly focusing on Codefast packages and Playwright. This commit removes the exception for the 'unicorn/no-process-exit' rule in ESLint configuration. It ensures compatibility with recent package updates and maintains code quality standards.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,4 @@ import { config } from '@codefast/eslint-config/next';
 import query from '@tanstack/eslint-plugin-query';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
-  ...config,
-  ...query.configs['flat/recommended'],
-  {
-    rules: {
-      'unicorn/no-process-exit': 'off',
-    },
-  },
-];
+export default [...config, ...query.configs['flat/recommended']];
